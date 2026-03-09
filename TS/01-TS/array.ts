19d44f833bf4cd94494779a43891d70397996d88
aviation5770 <aviation5770@gmail.com>
2026-03-09 16:09:52 +0900

    array 과제

--- TS/01-TS/array.ts
+++ TS/01-TS/array.ts
@@ -1,4 +1,4 @@
-const array = ["Hello", "World", "Good", "Days"];
-const array2 = array.map((i) => {
+const array:string[] = ["Hello", "World", "Good", "Days"];
+const array2:number[] = array.map((i:string):number => { // i는 string 타입, 반환값은 number 타입
   return i.length;
 });
