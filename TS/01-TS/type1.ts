f341c5d4b132272a8d18e23d35f83fdd745fc5a5
aviation5770 <aviation5770@gmail.com>
2026-03-09 15:56:24 +0900

    type 과제

---
+++ TS/01-TS/type1.ts
@@ -0,0 +1,10 @@
+const value1:number = 1;
+const value2:number = value1 + 1;
+const value3:string = "Hello";
+
+function add(arg1:number, arg2:number) {
+  return arg2 + arg2;
+}
+
+const result:number = add(1, 2);
+const check:boolean = result > 3;
