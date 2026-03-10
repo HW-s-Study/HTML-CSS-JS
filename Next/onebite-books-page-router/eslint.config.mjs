8aa5d7b8e1caaf436cdbf260b46171d01029d77c
aviation5770 <aviation5770@gmail.com>
2026-03-10 10:10:22 +0900

    설정

--- Next/onebite-books-page-router/eslint.config.mjs
+++ Next/onebite-books-page-router/eslint.config.mjs
@@ -13,6 +13,13 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+
+    rules:{
+      "@typescript-eslint/no-unused-vars":"off",
+      "@typescript-eslint/no-explicit-any":"warn"
+    }
+  } // 규칙을 좀 풀어주는 느낌
 ]);
 
 export default eslintConfig;
