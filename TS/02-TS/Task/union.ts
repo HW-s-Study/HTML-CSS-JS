64ca8fbfbb9e68dabcff01923d5fe573ac49b498
aviation5770 <aviation5770@gmail.com>
2026-03-11 08:56:57 +0900

    union 과제

---
+++ TS/02-TS/Task/union.ts
@@ -0,0 +1,10 @@
+console.log("Hello, World!");
+function getLength(a: string | string[]): number {
+  return a.length;
+}
+
+/* 이 자리에 함수 정의 */
+
+console.log(getLength("Hello")); // 5
+const arr = ["Hello", "World"];
+console.log(getLength(arr)); // 2
