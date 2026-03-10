944e3da1feb8a1e3650ba6376a51960c29c016c4
aviation5770 <aviation5770@gmail.com>
2026-03-10 10:08:50 +0900

    설정

--- Next/onebite-books-page-router/next.config.ts
+++ Next/onebite-books-page-router/next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  reactStrictMode: true,
+  reactCompiler: true, // Next16에 추가. 컴포넌트를 자동으로 메모이제이션
+  reactStrictMode: false, // 잠재적인 오류 찾도록 두 번 렌더링 하는 옵션
 };
 
 export default nextConfig;
