959a1fdd2f5c3a87e856ec71b763744be86049d6
aviation5770 <aviation5770@gmail.com>
2026-03-11 10:00:16 +0900

    쿼리 스트링 사용

--- Next/onebite-books-page-router/src/pages/book/[id].tsx
+++ Next/onebite-books-page-router/src/pages/book/[id].tsx
@@ -1,7 +1,10 @@
+import {useRouter} from "next/router";
+
 export default function Page() {
+  const router = useRouter();
   return (
     <div>
-      <h1>책 상세 페이지</h1>
+      <h1>{router.query.id} 도서 상세 페이지</h1>
     </div>
   );
 }
