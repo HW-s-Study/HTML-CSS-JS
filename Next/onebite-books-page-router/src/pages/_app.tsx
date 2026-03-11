d16988d02e758da0c3517fcad9fb7fce6316ce76
aviation5770 <aviation5770@gmail.com>
2026-03-11 10:25:27 +0900

    라우터 객체를 이용한 네비게이팅

--- Next/onebite-books-page-router/src/pages/_app.tsx
+++ Next/onebite-books-page-router/src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-
+  const router = useRouter();
   return (
     <>
-      <header>헤더</header>
+      <header>
+        <Link href={"/"}>홈</Link>
+        &nbsp;
+        <Link href={"/search"}>검색</Link>
+        &nbsp;
+        
+        <button onClick={() => {
+          router.push("/book/1");
+        }}>
+          1번 도서
+        </button>
+      </header>
       <Component {...pageProps} />
     </>
   );
