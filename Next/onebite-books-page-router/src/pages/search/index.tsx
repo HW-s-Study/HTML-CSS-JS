97d4dd883cd55b5530f008f47e46008f6d5e44ff
aviation5770 <aviation5770@gmail.com>
2026-03-11 09:59:00 +0900

    쿼리 스트링 사용

--- Next/onebite-books-page-router/src/pages/search/index.tsx
+++ Next/onebite-books-page-router/src/pages/search/index.tsx
@@ -1,8 +1,11 @@
-export default function Page() {
+import {useRouter} from "next/router";
 
+export default function Page() {
+  const router = useRouter();
   return (
     <div>
-      <h1>검색 페이지입니다.</h1>
+      <h1>검색 : {router.query.q}</h1>    
     </div>
+    // q는 변수명으로 검색어를 담을 변수 /search?q=검색어 형태로 요청이 들어오면 q에 검색어가 담긴다
   );
 }
