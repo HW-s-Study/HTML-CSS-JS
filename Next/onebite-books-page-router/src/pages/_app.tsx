6c390980da7bcc22bd734518a76ff2b7dfe587a1
aviation5770 <aviation5770@gmail.com>
2026-03-12 15:17:06 +0900

    글로벌 레이아웃 과제

--- Next/onebite-books-page-router/src/pages/_app.tsx
+++ Next/onebite-books-page-router/src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import GlobalLayout from "@/components/global-layout";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import "@/styles/globals.css";
+import { NextPage } from "next";
+import { ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = Component.getLayout;
+type NextPageWithLayout = NextPage & {
+  getLayout: (page:ReactNode) => ReactNode
+}
+
+
+export default function App({ Component, pageProps }: AppProps & {Component:NextPageWithLayout}) {
+  const getLayout = Component.getLayout || ((page) => page); // getLayout이 없는 페이지는 전달받은 컴포넌트를 그대로 렌더링하도록 기본값 설정
   // console.log(getLayout);
   
   return (
