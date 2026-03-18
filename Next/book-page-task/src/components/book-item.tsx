6a75f5a62b7ea225a7ae946a9a3abccd0768ece7
aviation5770 <aviation5770@gmail.com>
2026-03-18 10:00:12 +0900

    인덱스 페이지 UI 구현

--- Next/book-page-task/src/components/book-item.tsx
+++ Next/book-page-task/src/components/book-item.tsx
@@ -1,6 +1,6 @@
 import type { BookData } from '@/types';
 import Link from 'next/link';
-import style from './book-item.module.scss';
+import style from './book-item.module.css';
 
 export default function BookItem( {
   id,
--- Next/book-page-task/src/pages/index.tsx
+++ Next/book-page-task/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import SearchbarLayout from "@/components/searchbar-layout";
 import { ReactNode } from "react";
 import s from "./index.module.css";
+import BookItem from "@/components/book-item";
+import books from "@/mock/books.json";
 
 export default function Home() {
 
@@ -8,9 +10,15 @@ export default function Home() {
     <div className={s.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
+        { books.map((book) => (
+          <BookItem key={`recommended-${book.id}`} { ...book } />
+        ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
+        { books.map((book) => (
+          <BookItem key={`all-${book.id}`} { ...book } />
+        ))}
       </section>
     </div>
   );
