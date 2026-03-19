a8b3b7ea39483d1d8711abbcb3a40eb7ad80fffb
aviation5770 <aviation5770@gmail.com>
2026-03-19 14:42:04 +0900

    상세페이지 추가 및 경로 문제 수정

--- Next/book-page-task/src/pages/book/[id].tsx
+++ Next/book-page-task/src/pages/book/[id].tsx
@@ -1,10 +1,32 @@
-import {useRouter} from "next/router";
+// import {useRouter} from "next/router";
+import style from "./[id].module.css";
+import books from "@/mock/books.json";
 
 export default function Page() {
-  const router = useRouter();
+  // const router = useRouter();
+  const {
+    id,
+    title,
+    subTitle,
+    description,
+    author,
+    publisher,
+    coverImgUrl,
+  } = books[0];
+
   return (
-    <div>
-      <h1>{router.query.id} 도서 상세 페이지</h1>
+    <div className={style.container}>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url(${coverImgUrl})` }}>
+          <img src={coverImgUrl} />
+        </div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+        <div className={style.description}>{description}</div>
     </div>
   );
 }
