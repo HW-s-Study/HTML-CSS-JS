2e6e4ad60e286b9f3a6d2a2db3d02e5171d4c0f5
aviation5770 <aviation5770@gmail.com>
2026-03-26 11:12:32 +0900

    검색 페이지 메타 태그 설정

--- Next/onebite-books/src/pages/index.tsx
+++ Next/onebite-books/src/pages/index.tsx
@@ -5,6 +5,7 @@ import BookItem from "@/components/book-item";
 // import books from "@/mock/books.json";
 // import { InferGetServerSidePropsType } from 'next';
 import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
+import Head from "next/head";
 
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
@@ -29,19 +30,27 @@ export default function Home({
   // }, []);
 
   return (
-    <div className={s.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        { randomBooks.map((book) => (
-          <BookItem key={`recommended-${book.id}`} { ...book } />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        { allBooks.map((book) => (
-          <BookItem key={`all-${book.id}`} { ...book } />
-        ))}
-      </section>
+    <div>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:title" content="한입북스-검색결과" />
+        <meta property="og:description" content="한입북스에 등록된 도서들을 만나보세요" />
+        <meta property="og:image" content="/thumbnail.png" />
+      </Head>
+      <div className={s.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          { randomBooks.map((book) => (
+            <BookItem key={`recommended-${book.id}`} { ...book } />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          { allBooks.map((book) => (
+            <BookItem key={`all-${book.id}`} { ...book } />
+          ))}
+        </section>
+      </div>
     </div>
   );
 }
--- Next/onebite-books/src/pages/search/index.tsx
+++ Next/onebite-books/src/pages/search/index.tsx
@@ -8,6 +8,7 @@ import BookItem from "@/components/book-item";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import type { BookData } from "@/types";
+import Head from "next/head";
 
 // export async function getServerSideProps(context:GetServerSidePropsContext) {
 //   const q = context.query.q;
@@ -44,6 +45,12 @@ export default function Page() {
 
   return (
     <div>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:title" content="한입북스 - 검색결과" />
+        <meta property="og:description" content="한입북스에 등록된 도서들을 만나보세요" />
+        <meta property="og:image" content="/thumbnail.png" />
+      </Head>
       {books.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
