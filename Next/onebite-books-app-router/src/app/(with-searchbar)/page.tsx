562b92b73475ddc1306310abc34b6cfc35ab81b6
aviation5770 <aviation5770@gmail.com>
2026-04-01 10:33:02 +0900

    데이터 페칭 1

--- Next/onebite-books-app-router/src/app/(with-searchbar)/page.tsx
+++ Next/onebite-books-app-router/src/app/(with-searchbar)/page.tsx
@@ -1,19 +1,23 @@
-import books from "@/mock/books.json"
+// import books from "@/mock/books.json"
 import BookItem from "@/components/book-item";
 import style from "./page.module.css"
+import { BookData } from "@/types";
 
-export default function Home() {
+export default async function Home() {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER}/book`);
+    const allBooks: BookData[] = await response.json();
+    
     return (
         <div className={style.container}>
             <section>
                 <h3>지금 추천하는 도서</h3>
-                {books.map((book)=>(
+                {allBooks.map((book)=>(
                     <BookItem key={book.id} {...book}/>
                 ))}
             </section>
             <section>
                 <h3>등록된 모든 도서</h3>
-                {books.map((book)=>(
+                {allBooks.map((book)=>(
                     <BookItem key={book.id} {...book}/>
                 ))}
             </section>
