e3d1f0fd78d90577352336c16cb825ddf03d3193
aviation5770 <aviation5770@gmail.com>
2026-04-02 14:41:39 +0900

    reco, all 함수 추가 및 적용

--- Next/onebite-books-app-router/src/app/(with-searchbar)/page.tsx
+++ Next/onebite-books-app-router/src/app/(with-searchbar)/page.tsx
@@ -3,23 +3,53 @@ import BookItem from "@/components/book-item";
 import style from "./page.module.css"
 import { BookData } from "@/types";
 
-export default async function Home() {
+async function AllBooks() {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER}/book`);
     const allBooks: BookData[] = await response.json();
-    
+    return (
+        <div>
+            {allBooks.map((book) => (
+                <BookItem key={book.id} {...book} />
+            ))}
+        </div>
+    );
+}
+
+async function RecoBooks() {
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_SERVER}/book/random`
+    );
+
+    const randomBooks: BookData[] = await response.json();
+    return (
+        <div>
+            {randomBooks.map((book) => (
+                <BookItem key={book.id} {...book} />
+            ))}
+        </div>
+    );
+}
+
+// export default async function Home() {
+    // const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER}/book`);
+    // const allBooks: BookData[] = await response.json();
+export default function Home() {
+
     return (
         <div className={style.container}>
             <section>
                 <h3>지금 추천하는 도서</h3>
-                {allBooks.map((book)=>(
+                <RecoBooks />
+                {/* {allBooks.map((book)=>(
                     <BookItem key={book.id} {...book}/>
-                ))}
+                ))} */}
             </section>
             <section>
                 <h3>등록된 모든 도서</h3>
-                {allBooks.map((book)=>(
+                <AllBooks />
+                {/* {allBooks.map((book)=>(
                     <BookItem key={book.id} {...book}/>
-                ))}
+                ))} */}
             </section>
         </div>
     );
