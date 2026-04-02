a0b2b9cfcdbb17baa171851c15d768396a316ccd
aviation5770 <aviation5770@gmail.com>
2026-04-02 14:48:19 +0900

    allbooks 수정

--- Next/onebite-books-app-router/src/app/(with-searchbar)/page.tsx
+++ Next/onebite-books-app-router/src/app/(with-searchbar)/page.tsx
@@ -4,23 +4,32 @@ import style from "./page.module.css"
 import { BookData } from "@/types";
 
 async function AllBooks() {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER}/book`);
-    const allBooks: BookData[] = await response.json();
-    return (
-        <div>
-            {allBooks.map((book) => (
-                <BookItem key={book.id} {...book} />
-            ))}
-        </div>
-    );
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER}/book`);
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        const allBooks: BookData[] = await response.json();
+
+        return (
+            <div>
+                {allBooks.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </div>
+        );
+    } catch (err) {
+        console.log(err);
+        return <div>오류가 발생했습니다.</div>;
+    }
 }
 
 async function RecoBooks() {
     const response = await fetch(
         `${process.env.NEXT_PUBLIC_API_SERVER}/book/random`
     );
-
     const randomBooks: BookData[] = await response.json();
+
     return (
         <div>
             {randomBooks.map((book) => (
