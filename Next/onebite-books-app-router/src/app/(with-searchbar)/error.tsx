55fb67a8f2f1d68c39305f6b57dc9b02848afd5d
aviation5770 <aviation5770@gmail.com>
2026-04-07 10:35:22 +0900

    error 메서드

--- Next/onebite-books-app-router/src/app/(with-searchbar)/error.tsx
+++ Next/onebite-books-app-router/src/app/(with-searchbar)/error.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 interface Props {
   error: Error & { digest?: string };
   reset: () => void;
 }
 
 export default function Error({ error, reset }: Props) {
+  const router = useRouter();
   console.log(error);
 
   return (
@@ -13,7 +16,8 @@ export default function Error({ error, reset }: Props) {
       <h1>오류가 발생했습니다.</h1>
       <button
         onClick={() => {
-          window.location.reload();
+          router.refresh();
+          reset();
         }}
       >다시 시도</button>
     </div>
